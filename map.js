722c4d8b61d9b5ff672c8c555a2329d1b54b9493
Xun Liu <liuxun1230@hotmail.com>
2020-07-07 10:22:35 -0400

    TimeLineAdded

--- map.js
+++ map.js
@@ -34,14 +34,14 @@ var mapZoom = 12.25;
 // 3. Creating a legend
 // See example tutorial at https://docs.mapbox.com/help/tutorials/choropleth-studio-gl-pt-2/#create-arrays-of-intervals-and-colors
 
-    var layers = [ // an array of the possible values you want to show in your legend
-        'Civic Spaces', // Civic Spaces.png
-        'Community Park', // Community Park.png
-        'Neighborhood Park', // Neighborhood Park.png
-        'Cemetery',
-        'Urban Park',
-        'Regional Park'
-    ];
+//    var layers = [ // an array of the possible values you want to show in your legend
+//        'Civic Spaces', // Civic Spaces.png
+//        'Community Park', // Community Park.png
+//        'Neighborhood Park', // Neighborhood Park.png
+//        'Cemetery',
+//        'Urban Park',
+//        'Regional Park'
+//    ];
 
     var colors = [ // an array of the color values for each legend item
         '#800000',
@@ -53,16 +53,16 @@ var mapZoom = 12.25;
     ];
 
     // for loop to create individual legend items
-    for (i=0; i<layers.length; i++) {
-        var layer =layers[i]; // name of the current legend item, from the layers array
-        var color =colors[i]; // color value of the current legend item, from the colors array 
-        
-        var itemHTML = "<div><span class='legend-key'></span><span>" + layer + "</span></div>"; // create the HTML for the legend element to be added
-
-        var item = $(itemHTML).appendTo("#legend"); // add the legend item to the legend
-        var legendKey = $(item).find(".legend-key"); // find the legend key (colored circle) for the current item
-        legendKey.css("background", color); // change the background color of the legend key
-    }
+//    for (i=0; i<layers.length; i++) {
+//        var layer =layers[i]; // name of the current legend item, from the layers array
+//        var color =colors[i]; // color value of the current legend item, from the colors array 
+//        
+//        var itemHTML = "<div><span class='legend-key'></span><span>" + layer + "</span></div>"; // create the HTML for the legend element to be added
+//
+//        var item = $(itemHTML).appendTo("#legend"); // add the legend item to the legend
+//        var legendKey = $(item).find(".legend-key"); // find the legend key (colored circle) for the current item
+//        legendKey.css("background", color); // change the background color of the legend key
+//    }
 
 
 // --------------------------------------------------------
@@ -140,12 +140,10 @@ var mapZoom = 12.25;
         // [layerMachineName, layerDisplayName]
         // layerMachineName is the layer name as written in your Mapbox Studio map layers panel
         // layerDisplayName is the way you want the layer's name to appear in the layers control on the website
-        ['cville-bus-stops', 'Bus Stops'],                      // layers[0]
-        ['cville-parks', 'Parks'],                              // layers[1][1] = 'Parks'
-        ['cville-bike-lanes', 'Bike Lanes'],     
-        ['cville-bus-stops-heatmap', 'Bus Stop Heatmap'],
-        ['background', 'Map background']
-        // add additional live data layers here as needed
+        ['Point_O', 'Pizza Hut'],                      // layers[0]
+        ['Point_D', 'Delivery Destinations'],         // layers[1][1] = 'Parks'
+        ['Point_D_Heatmap', 'Destination Heatmap'],     
+        ['Shops', 'Surrounding Shops']        // add additional live data layers here as needed
     ]; 
 
     // functions to perform when map loads
@@ -182,27 +180,27 @@ var mapZoom = 12.25;
 // -------------------------------------------------------- 
 // 7. Change a layer's style
 // See example at https://www.mapbox.com/mapbox-gl-js/example/color-switcher/
-    
-    var swatches = $("#swatches");
-
-    var colors = [  // an array of color options for the bus stop ponts
-        '#ffd000',
-        '#f00',
-    ]; 
-
-    var layer = 'cville-bus-stops';
-
-    colors.forEach(function(color) {
-        var swatch = $("<button class='swatch'></button>").appendTo(swatches);
-
-        $(swatch).css('background-color', color); 
-
-        $(swatch).on('click', function() {
-            map.setPaintProperty(layer, 'circle-color', color); // 'circle-color' is a property specific to a circle layer. Read more about what values to use for different style properties and different types of layers at https://www.mapbox.com/mapbox-gl-js/style-spec/#layers
-        });
-
-        $(swatches).append(swatch);
-    });
+//    
+//    var swatches = $("#swatches");
+//
+//    var colors = [  // an array of color options for the bus stop ponts
+//        '#ffd000',
+//        '#f00',
+//    ]; 
+//
+//    var layer = 'cville-bus-stops';
+//
+//    colors.forEach(function(color) {
+//        var swatch = $("<button class='swatch'></button>").appendTo(swatches);
+//
+//        $(swatch).css('background-color', color); 
+//
+//        $(swatch).on('click', function() {
+//            map.setPaintProperty(layer, 'circle-color', color); // 'circle-color' is a property specific to a circle layer. Read more about what values to use for different style properties and different types of layers at https://www.mapbox.com/mapbox-gl-js/style-spec/#layers
+//        });
+//
+//        $(swatches).append(swatch);
+//    });
 
 // -------------------------------------------------------- 
 // 8. Scroll to zoom through sites
@@ -210,7 +208,7 @@ var mapZoom = 12.25;
     
     // A JavaScript object containing all of the data for each site "chapter" (the sites to zoom to while scrolling)
     var chapters = {
-        'darden-towe': {
+        'chapter01': {
             name: "01_Overview",
             description: "Ut nisl quam, fringilla efficitur elementum in, congue vel mi. Nullam consequat pharetra nibh, non accumsan nisl cursus sed. Pellentesque at ex lacus. Ut fringilla nunc id leo maximus ullamcorper. Donec volutpat placerat accumsan. Nulla id luctus diam. Aliquam tincidunt pulvinar mattis. Donec tempor, massa vel vehicula feugiat, diam sem suscipit nisi, eu tempor turpis lorem ac ipsum. Proin quis lectus mattis enim luctus faucibus sit amet vel metus. Etiam luctus nunc eget velit vestibulum posuere. Maecenas enim velit, elementum a suscipit vel, bibendum in odio. Nunc porta, eros nec vehicula pretium, tellus sapien fermentum risus, a pulvinar elit libero ut nisi. Nunc interdum lacus eu ornare dapibus. Suspendisse vitae diam eu turpis venenatis tempor. Ut sodales vel ex finibus facilisis. Nunc hendrerit, augue eget vulputate pellentesque, nibh erat imperdiet justo, id iaculis risus sem commodo urna.",
             //imagepath: "img/Darden Towe.jpg",
@@ -219,7 +217,7 @@ var mapZoom = 12.25;
             zoom: 12,
             pitch: 0
         },
-        'mcguffey-park': {
+        'chapter02': {
             name: "02_Zoom in",
             description: "Aliquam mollis consequat libero, at egestas mi facilisis in. Maecenas sed porta arcu, nec mattis ligula. Sed a porta arcu. Aliquam vel nulla ac orci volutpat ullamcorper. Duis quis auctor urna. Duis id felis vel velit sagittis bibendum. Praesent rutrum velit vel est iaculis, et viverra sapien placerat. Suspendisse potenti. In interdum eu lorem ac cursus. Integer pulvinar lacus nec metus consequat vehicula. Aliquam efficitur vitae neque sed aliquam. Fusce interdum tempor neque vel interdum. Praesent dapibus sollicitudin arcu id finibus. Mauris risus magna, egestas in tristique et, egestas id arcu. Proin leo urna, sollicitudin non mattis in, tempor non nisi. Praesent commodo nibh sit amet dapibus egestas.",
             //imagepath: "img/McGuffey Park.jpg",
@@ -228,8 +226,8 @@ var mapZoom = 12.25;
             zoom: 11.87,
             pitch: 50
         },
-        'mcintire-park': {
-            name: "McIntire Park",
+        'chapter03': {
+            name: "03???",
             description: "Fusce iaculis nulla ut augue posuere, sit amet vestibulum quam elementum. Integer quis varius sem. Mauris fermentum tempus congue. Nulla facilisi. Vestibulum congue cursus tempor. Sed sit amet venenatis magna. Duis fermentum ligula eget auctor eleifend. Aenean ullamcorper arcu et diam pharetra, a pretium lectus porttitor. Donec non lacinia est. Nullam nec felis turpis. Curabitur hendrerit porta dolor, vitae vehicula est dictum id. Ut sollicitudin lectus est, et egestas felis tempus eu. Sed at dictum ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam et diam rutrum orci placerat euismod quis vitae dui. Praesent aliquam, quam ac suscipit laoreet, metus nulla mattis justo, et egestas lectus eros ac nulla.",
             //imagepath: "img/McIntire Park.jpg",
             bearing: 20,
@@ -237,8 +235,8 @@ var mapZoom = 12.25;
             zoom: 15,
             pitch: 50
         },
-        'rivanna-river': {
-            name: "Rivanna River",
+        'chapter04': {
+            name: "04???",
             description: "Aenean rutrum finibus ex, quis mollis ante eleifend in. Vestibulum faucibus augue tellus, ac auctor tellus maximus sit amet. Nulla quis rutrum felis. Nullam a facilisis mi, in pretium orci. Vestibulum tempus odio et accumsan lacinia. Duis tempus, dolor sit amet tristique tempus, nisl neque tristique lacus, quis viverra est risus id quam. Donec condimentum massa vitae dui consectetur vehicula. Vivamus interdum nisi sed blandit fermentum. Proin a magna et est varius euismod non quis turpis. Cras rhoncus, nulla non faucibus vestibulum, felis nunc finibus nisi, dictum sollicitudin nibh leo non lorem. Donec ut nulla id nunc elementum luctus. Fusce sed justo ac metus pretium auctor ut eget magna. Vestibulum rhoncus nibh sit amet varius tincidunt.",
             //imagepath: "img/Rivanna River.jpg",
             bearing: 0,
@@ -248,13 +246,13 @@ var mapZoom = 12.25;
         }
     };
 
-    console.log(chapters['darden-towe']['name']);
+    console.log(chapters['chapter01']['name']);
     console.log(Object.keys(chapters)[0]);
 
     // Add the chapters to the #chapters div on the webpage
     for (var key in chapters) {
         var newChapter = $("<div class='chapter' id='" + key + "'></div>").appendTo("#chapters");
-        var chapterHTML = $("<h1>" + chapters[key]['name'] + "</h1>" + "<p>" + chapters[key]['description'] + "</p>"+"<br><br><br><br><br><br><br><br><br><br>").appendTo(newChapter);
+        var chapterHTML = $("<h3>" + chapters[key]['name'] + "</h3>" + "<p>" + chapters[key]['description'] + "</p>"+"<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>").appendTo(newChapter);
     }
 
 //If include images
@@ -332,3 +330,22 @@ var mapZoom = 12.25;
 
     });
 
+
+// -------------------------------------------------------- 
+// 10. Time Slider
+document.getElementById('slider').addEventListener('input', function(e) {
+  var hour = parseInt(e.target.value);
+  // update the map
+  map.setFilter('Point_D', ['==', ['get', 'Hrs'], hour]);
+  map.setFilter('Point_D_Heatmap', ['==', ['get', 'Hrs'], hour]);
+
+
+
+  // converting 0-23 hour to AMPM format
+  var ampm = hour >= 12 ? 'PM' : 'AM';
+  var hour12 = hour % 12 ? hour % 12 : 12;
+
+  // update text in the UI
+  document.getElementById('active-hour').innerText = hour12 + ampm;
+});
+ 
