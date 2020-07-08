b4bb490a832b79f569f411db87d8f46773f5a1d8
Xun Liu <liuxun1230@hotmail.com>
2020-07-07 22:51:18 -0400

    submission

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 // Global variables
-var mapCenter = [114.169028, 22.372134];
-var mapZoom = 10.57;
+var mapCenter = [114.131459, 22.414532];
+var mapZoom = 10.6;
 
 
 // --------------------------------------------------------
@@ -12,7 +12,7 @@ var mapZoom = 10.57;
     	container: 'map', // this is the ID of the div in index.html where the map should go
         center: mapCenter, // set the centerpoint of the map programatically. Note that this is [longitude, latitude]!
         zoom: mapZoom, // set the default zoom programatically
-    	style: 'mapbox://styles/xunliu/ckcc88mte1thp1imk87qlle7l', // replace this value with the style URL from Mapbox Studio
+    	style: 'mapbox://styles/xunliu/ckccq2m896rrz1jnzvd5gzijy', // replace this value with the style URL from Mapbox Studio
     });
 
 
@@ -90,10 +90,13 @@ var mapZoom = 10.57;
         ['Shops', 'Surrounding Shops'],
         ['Circle_O', 'Value of Each Store'],
         ['Bar_O', 'value of Each Store 3D'],
-        ['YuenLangDistrictOD', 'Original Yuen Lang District Delivery '],
         ['Optimized_Sample30KOD', 'Original Delivery Routes'],
-        ['YuenLangDistrictO1D', 'Optimized Yuen Lang District Delivery '],
         ['Optimized_Sample30KO1D', 'Optimized Delivery Routes'],
+        ['Buffer_1k', 'Original Location 1k Buffer'],
+        ['Buffer_3k', 'Original Location 3k Buffer'],
+        ['Newstores', 'Suggested Store Location'],
+        ['Newstore_1k', 'Suggested Location 1k Buffer'],
+        ['Newstore_3k', 'Suggested Location 3k Buffer']
 
 
         // add additional live data layers here as needed
@@ -106,7 +109,7 @@ var mapZoom = 10.57;
         for (i=0; i<layers.length; i++) {
 
             // add a button for each layer
-            $("#layers-control").append("<a href='#' class='active button-default' id='" + layers[i][0] + "'>" + layers[i][1] + "</a>"); // see http://api.jquery.com/append/
+            $("#layers-control").append("<a href='#' class='button-default' id='" + layers[i][0] + "'>" + layers[i][1] + "</a>"); // see http://api.jquery.com/append/
         }
 
         // show/hide layers when button is clicked
@@ -143,65 +146,115 @@ var mapZoom = 10.57;
     // A JavaScript object containing all of the data for each site "chapter" (the sites to zoom to while scrolling)
     var chapters = {
         'chapter01': {
-            name: "01_Overview",
-            description: "Ut nisl quam, fringilla efficitur elementum in, congue vel mi. Nullam consequat pharetra nibh, non accumsan nisl cursus sed. Pellentesque at ex lacus. Ut fringilla nunc id leo maximus ullamcorper. Donec volutpat placerat accumsan. Nulla id luctus diam. Aliquam tincidunt pulvinar mattis. Donec tempor, massa vel vehicula feugiat, diam sem suscipit nisi, eu tempor turpis lorem ac ipsum. Proin quis lectus mattis enim luctus faucibus sit amet vel metus. Etiam luctus nunc eget velit vestibulum posuere. Maecenas enim velit, elementum a suscipit vel, bibendum in odio. Nunc porta, eros nec vehicula pretium, tellus sapien fermentum risus, a pulvinar elit libero ut nisi. Nunc interdum lacus eu ornare dapibus. Suspendisse vitae diam eu turpis venenatis tempor. Ut sodales vel ex finibus facilisis. Nunc hendrerit, augue eget vulputate pellentesque, nibh erat imperdiet justo, id iaculis risus sem commodo urna.",
+            name: "01: Current Problems",
+            description: "Current Delivery Locations:<br> This maps shows all currrent delivery locations, while the darker red is the distance to the PizzaHut, radius of each circle represents the value of each delivery.",
             //imagepath: "img/Darden Towe.jpg",
             bearing: 0,
-            center: [114.067240, 22.464007],
-            zoom: 12,
+            center: [114.131459, 22.414532],
+            zoom: 10.6,
             pitch: 0,
-            layersVis:['Point_D','Circle_O'],
             
         },
         'chapter02': {
-            name: "02_Zoom in",
-            description: "Aliquam mollis consequat libero, at egestas mi facilisis in. Maecenas sed porta arcu, nec mattis ligula. Sed a porta arcu. Aliquam vel nulla ac orci volutpat ullamcorper. Duis quis auctor urna. Duis id felis vel velit sagittis bibendum. Praesent rutrum velit vel est iaculis, et viverra sapien placerat. Suspendisse potenti. In interdum eu lorem ac cursus. Integer pulvinar lacus nec metus consequat vehicula. Aliquam efficitur vitae neque sed aliquam. Fusce interdum tempor neque vel interdum. Praesent dapibus sollicitudin arcu id finibus. Mauris risus magna, egestas in tristique et, egestas id arcu. Proin leo urna, sollicitudin non mattis in, tempor non nisi. Praesent commodo nibh sit amet dapibus egestas.",
+            name: "",
+            description: "Heatmap of Current Delivery Locations:<br>All delivery locations/per delivery are visaulized as heatmap. As many locations have multiple delivery through the survey period.",
             //imagepath: "img/McGuffey Park.jpg",
-            bearing: 68,
-            center: [114.010026, 22.429198],
-            zoom: 11.87,
-            pitch: 50,
-            layersVis:['Point_D','Circle_O'],
-
+            bearing: 0,
+            center: [114.131459, 22.414532],
+            zoom: 10.6,
+            pitch: 0,
         },
-        'chapter03': {
-            name: "03???",
-            description: "Fusce iaculis nulla ut augue posuere, sit amet vestibulum quam elementum. Integer quis varius sem. Mauris fermentum tempus congue. Nulla facilisi. Vestibulum congue cursus tempor. Sed sit amet venenatis magna. Duis fermentum ligula eget auctor eleifend. Aenean ullamcorper arcu et diam pharetra, a pretium lectus porttitor. Donec non lacinia est. Nullam nec felis turpis. Curabitur hendrerit porta dolor, vitae vehicula est dictum id. Ut sollicitudin lectus est, et egestas felis tempus eu. Sed at dictum ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam et diam rutrum orci placerat euismod quis vitae dui. Praesent aliquam, quam ac suscipit laoreet, metus nulla mattis justo, et egestas lectus eros ac nulla.",
-            //imagepath: "img/McIntire Park.jpg",
-            bearing: 20,
-            center: [ 113.989629, 22.429796],
-            zoom: 15,
-            pitch: 50,
-            layersVis:['Point_D','Circle_O'],
-
+         'chapter03': {
+            name: "",
+            description: "Current Delivery Locations Changing Through Time:<br> Visualization of current delivery data within 24 hours. You can change the slider to navigate to different time period.",
+            //imagepath: "img/McGuffey Park.jpg",
+            bearing: 0,
+            center: [114.131459, 22.414532],
+            zoom: 10.6,
+            pitch: 0,
         },
         'chapter04': {
-            name: "04???",
-            description: "Aenean rutrum finibus ex, quis mollis ante eleifend in. Vestibulum faucibus augue tellus, ac auctor tellus maximus sit amet. Nulla quis rutrum felis. Nullam a facilisis mi, in pretium orci. Vestibulum tempus odio et accumsan lacinia. Duis tempus, dolor sit amet tristique tempus, nisl neque tristique lacus, quis viverra est risus id quam. Donec condimentum massa vitae dui consectetur vehicula. Vivamus interdum nisi sed blandit fermentum. Proin a magna et est varius euismod non quis turpis. Cras rhoncus, nulla non faucibus vestibulum, felis nunc finibus nisi, dictum sollicitudin nibh leo non lorem. Donec ut nulla id nunc elementum luctus. Fusce sed justo ac metus pretium auctor ut eget magna. Vestibulum rhoncus nibh sit amet varius tincidunt.",
-            //imagepath: "img/Rivanna River.jpg",
-            bearing: 0,
-            center: [ 113.989629, 22.429796],
-            zoom: 16.13,
-            pitch: 25
+            name: "",
+            description: "Demand From Different PizzaHut Stores:<br> Different Pizzahut Stores has significant uneven demand for delivery. The radius of the circles shows the total value of all delivery from each Pizzahut Stores",
+            //imagepath: "img/McIntire Park.jpg",
+            bearing: 17.41,
+            center: [114.134088, 22.410199],
+            zoom: 11,
+            pitch: 60.00,
         },
         'chapter05': {
-            name: "05???",
-            description: "Aenean rutrum finibus ex, quis mollis ante eleifend in. Vestibulum faucibus augue tellus, ac auctor tellus maximus sit amet. Nulla quis rutrum felis. Nullam a facilisis mi, in pretium orci. Vestibulum tempus odio et accumsan lacinia. Duis tempus, dolor sit amet tristique tempus, nisl neque tristique lacus, quis viverra est risus id quam. Donec condimentum massa vitae dui consectetur vehicula. Vivamus interdum nisi sed blandit fermentum. Proin a magna et est varius euismod non quis turpis. Cras rhoncus, nulla non faucibus vestibulum, felis nunc finibus nisi, dictum sollicitudin nibh leo non lorem. Donec ut nulla id nunc elementum luctus. Fusce sed justo ac metus pretium auctor ut eget magna. Vestibulum rhoncus nibh sit amet varius tincidunt.",
-            //imagepath: "img/Rivanna River.jpg",
-            bearing: 0,
-            center: [ 113.989629, 22.429796],
-            zoom: 16.13,
-            pitch: 25
+            name: "",
+            description: "Zoomed in to Yuen Long District:<br> Lets zoom in to Yuen Long District to visualize the current delivery systems.",
+            //imagepath: "img/McIntire Park.jpg",
+            bearing: -8.56,
+            center: [114.014922, 22.441275],
+            zoom:12.95,
+            pitch: 60.00,
         },
         'chapter06': {
-            name: "06???",
-            description: "Aenean rutrum finibus ex, quis mollis ante eleifend in. Vestibulum faucibus augue tellus, ac auctor tellus maximus sit amet. Nulla quis rutrum felis. Nullam a facilisis mi, in pretium orci. Vestibulum tempus odio et accumsan lacinia. Duis tempus, dolor sit amet tristique tempus, nisl neque tristique lacus, quis viverra est risus id quam. Donec condimentum massa vitae dui consectetur vehicula. Vivamus interdum nisi sed blandit fermentum. Proin a magna et est varius euismod non quis turpis. Cras rhoncus, nulla non faucibus vestibulum, felis nunc finibus nisi, dictum sollicitudin nibh leo non lorem. Donec ut nulla id nunc elementum luctus. Fusce sed justo ac metus pretium auctor ut eget magna. Vestibulum rhoncus nibh sit amet varius tincidunt.",
+            name: "02: An AI Enabled Demand Prediction",
+            description: " Delivery Network Optimization System:<br> that integrates internal analysis (e.g. food demand, delivery fleet, inventory capacity, customer profiles) and external analysis about uncertainties and trends.",
             //imagepath: "img/Rivanna River.jpg",
             bearing: 0,
-            center: [ 113.989629, 22.429796],
-            zoom: 16.13,
-            pitch: 25
+            center: [114.131459, 22.414532],
+            zoom: 11,
+            pitch: 0,
+        },
+        'chapter07': {
+            name: "",
+            description: "Demand by Regions:<br> Demand by hours and region Food popularity by region Inventory improvements.",
+            //imagepath: "img/Rivanna River.jpg",
+            bearing: 0,
+            center: [114.131459, 22.414532],
+            zoom: 11,
+            pitch: 0,
+        },
+        'chapter08': {
+            name: "",
+            description: "Delivery Networks:<br>Inefficient dispatch Possibility of shared trips Global optimization.",
+            //imagepath: "img/Rivanna River.jpg",
+            bearing: 0,
+            center: [114.131459, 22.414532],
+            zoom: 11,
+            pitch: 0,
+        },    
+        'chapter09': {
+            name: "",
+            description: "Service Radius:<br> Inefficient location Possibility of new stores Global optimization.",
+            //imagepath: "img/Rivanna River.jpg",
+            bearing: -40.54,
+            center: [114.197381, 22.352413],
+            zoom:11.21,
+            pitch: 56.50,
+        },  
+        'chapter10': {
+            name: "03: Solution",
+            description: "Suggested Store Locations:<br>",
+            //imagepath: "img/Rivanna River.jpg",
+            bearing: -40.54,
+            center: [114.197381, 22.352413],
+            zoom:11.21,
+            pitch: 56.50,
+        },  
+         'chapter11': {
+            name: "",
+            description: "New Service Radius for Suggested Store Locations:<br>",
+            //imagepath: "img/Rivanna River.jpg",
+            bearing: 0,
+            center: [114.131459, 22.414532],
+            zoom: 10.6,
+            pitch: 0,
+        }, 
+          'chapter13': {
+            name: "",
+            description: "New Delivery Networks for Suggested Store Locations:<br>",
+            //imagepath: "img/Rivanna River.jpg",
+            bearing: 0,
+            center: [114.131459, 22.414532],
+            zoom: 11,
+            pitch: 0,
         }
+
     };
 
     console.log(chapters['chapter01']['name']);
@@ -210,7 +263,7 @@ var mapZoom = 10.57;
     // Add the chapters to the #chapters div on the webpage
     for (var key in chapters) {
         var newChapter = $("<div class='chapter' id='" + key + "'></div>").appendTo("#chapters");
-        var chapterHTML = $("<h3>" + chapters[key]['name'] + "</h3>" + "<p>" + chapters[key]['description'] + "</p>"+"<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>").appendTo(newChapter);
+        var chapterHTML = $("<h3>" +"<br>"+ chapters[key]['name'] +"<br><br>"+"</h3>" + "<p>" + chapters[key]['description'] + "</p>"+"<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>").appendTo(newChapter);
     }
 
 //If include images
